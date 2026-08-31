31de22f2ae23e2315f63f5451ad20bcee8b74521
Olga Kazarina <s-01132@student.german-uds.de>
2026-08-31 15:21:37 -0400

    Implement ModeView class for rendering effects
    Added ModeView class with filter and intensity management.

--- src/views/ModeView.h
+++ src/views/ModeView.h
@@ -1,5 +1,23 @@
 #pragma once
-class ModeView
-{
-};
 
+#include <opencv2/opencv.hpp>
+#include "../filters/BaseFilter.h"
+#include <string>
+
+// ModeView renders a focused single-preset environment with a vintage 35mm film border and intensity scale
+class ModeView {
+private:
+    BaseFilter* currentFilter; // Polymorphic pointer to active filter
+    float intensity;           // Scale: 0.0 (subtle) to 1.0 (full effect)
+    std::string modeName;
+
+public:
+    ModeView();
+    ~ModeView() = default;
+
+    void setFilter(BaseFilter* filter, const std::string& name);
+    void setIntensity(float val);
+    float getIntensity() const { return intensity; }
+
+    void draw(cv::Mat& canvas, const cv::Mat& currentFrame);
+};
