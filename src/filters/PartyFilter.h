cd942b891957bc4e9e2d4c5441c3820b7d782f92
Olga Kazarina <s-01132@student.german-uds.de>
2026-08-31 14:41:19 -0400

    Implement PartyFilter class with process method
    Added constructor, destructor, and overridden process method.

--- src/filters/PartyFilter.h
+++ src/filters/PartyFilter.h
@@ -1,7 +1,16 @@
 #pragma once
-#include "BaseFilter.h"
-class PartyFilter :
-    public BaseFilter
-{
-};
 
+#include "BaseFilter.h"
+#include <opencv2/opencv.hpp>
+
+// PartyFilter inherits from BaseFilter ("Is-A" Polymorphism relationship).
+// It overrides the pure virtual method 'process()' to apply neon and color-inversion effects.
+class PartyFilter : public BaseFilter
+{
+public:
+    PartyFilter() = default;
+    virtual ~PartyFilter() = default;
+
+    // Overridden processing function that transforms the input frame based on intensity (0.0 to 1.0)
+    cv::Mat process(const cv::Mat& input, float intensity) override;
+};
