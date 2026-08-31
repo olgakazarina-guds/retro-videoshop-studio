094b548ad217a8e1af67706ef20b20af4cacb387
Olga Kazarina <s-01132@student.german-uds.de>
2026-08-31 15:22:10 -0400

    Implement FilterStudioView class with ManualFilter
    Added FilterStudioView class to render manual slider sandbox.

--- src/views/FilterStudioView.h
+++ src/views/FilterStudioView.h
@@ -1,5 +1,17 @@
 #pragma once
-class FilterStudioView
-{
-};
 
+#include <opencv2/opencv.hpp>
+#include "../filters/ManualFilter.h"
+
+// FilterStudioView renders the manual slider sandbox (Filter-All Studio)
+class FilterStudioView {
+private:
+    ManualFilter manualFilter; // Owns the ManualFilter instance
+
+public:
+    FilterStudioView() = default;
+    ~FilterStudioView() = default;
+
+    ManualFilter& getFilter() { return manualFilter; }
+    void draw(cv::Mat& canvas, const cv::Mat& currentFrame);
+};
