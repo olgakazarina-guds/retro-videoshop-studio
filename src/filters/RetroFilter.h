c201fc342d43ff4a433711c94dcf730132b1c4f1
Olga Kazarina <s-01132@student.german-uds.de>
2026-09-02 10:09:40 -0400

    Define RetroFilter class with process method
    Implement process method for RetroFilter class.

--- src/filters/RetroFilter.h
+++ src/filters/RetroFilter.h
@@ -1,7 +1,15 @@
 #pragma once
-#include "BaseFilter.h"
-class RetroFilter :
-    public BaseFilter
-{
-};
 
+#include "BaseFilter.h"
+#include <opencv2/opencv.hpp>
+
+class RetroFilter : public BaseFilter {
+public:
+    RetroFilter() = default;
+    virtual ~RetroFilter() = default;
+
+    // Temporary pass-through until Esther implements sepia & grain
+    cv::Mat process(const cv::Mat& input, float intensity) override {
+        return input.empty() ? input : input.clone();
+    }
+};
