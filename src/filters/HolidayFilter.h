8dec847311607d6221aae81839bf3ad68914692b
Olga Kazarina <s-01132@student.german-uds.de>
2026-09-02 10:10:14 -0400

    Add HolidayFilter class with process method
    Implement process method for HolidayFilter class.

--- src/filters/HolidayFilter.h
+++ src/filters/HolidayFilter.h
@@ -1,7 +1,15 @@
 #pragma once
-#include "BaseFilter.h"
-class HolidayFilter :
-    public BaseFilter
-{
-};
 
+#include "BaseFilter.h"
+#include <opencv2/opencv.hpp>
+
+class HolidayFilter : public BaseFilter {
+public:
+    HolidayFilter() = default;
+    virtual ~HolidayFilter() = default;
+
+    // Temporary pass-through until Esther implements color channel shifts
+    cv::Mat process(const cv::Mat& input, float intensity) override {
+        return input.empty() ? input : input.clone();
+    }
+};
